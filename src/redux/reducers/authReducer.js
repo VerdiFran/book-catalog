54bccfc48cc313ae1758c840b4c2a9e7e13fbabf
VerdiFran <tmw.kva@yandex.ru>
2021-04-01 12:00:27 +0700

    Add login with firebase

---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,38 @@
+import firebase from '../../firebase'
+
+const SET_AUTH_DATA = 'SET-AUTH-DATA'
+
+const initialState = {
+    isAuth: false,
+    email: ''
+}
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_AUTH_DATA:
+            return {
+                ...state,
+                ...action.userData,
+                isAuth: action.isAuth
+            }
+        default:
+            return state
+    }
+}
+
+const setIsAuth = (isAuth, userData) => ({type: SET_AUTH_DATA, isAuth, userData})
+
+export const login = (email, password) => dispatch => {
+    const ref = firebase.firestore().collection('users')
+    ref.onSnapshot((querySnapshot) => {
+        for (const doc of querySnapshot) {
+            const {userEmail, userPassword} = doc.data()
+            if (userEmail === email && userPassword === password) {
+                dispatch(setIsAuth(true))
+                break
+            }
+        }
+    })
+}
+
+export default authReducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import catalogReducer from './reducers/catalogReducer'
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunk from 'redux-thunk'
+import authReducer from './reducers/authReducer'
 
 const reducers = combineReducers({
-    catalog: catalogReducer
+    catalog: catalogReducer,
+    auth: authReducer
 })
 
 const store = createStore(reducers, applyMiddleware(thunk))
