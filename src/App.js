7e811a9e5bbcef4bd46004505b89414b19144de5
VerdiFran <tmw.kva@yandex.ru>
2021-04-01 15:18:47 +0700

    Add BrowserRouter and routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {Provider} from 'react-redux'
 import store from './redux/store'
 import Header from './components/Header/Header'
 import {Layout} from 'antd'
+import {BrowserRouter, withRouter, Switch, Route, Redirect} from 'react-router-dom'
+import Login from './components/Login/Login'
+import Registration from './components/Registration/Registration'
 
 const App = () => {
     const {Content} = Layout
@@ -12,11 +15,33 @@ const App = () => {
             <div className="App">
                 <Header/>
                 <Content>
-                    <BookCatalogContainer/>
+                    <Switch>
+                        <Route
+                            path="/catalog"
+                            render={() => <BookCatalogContainer/>}
+                        />
+                        <Route
+                            path="/login"
+                            render={() => <Login/>}
+                        />
+                        <Route
+                            path="/register"
+                            render={() => <Registration/>}
+                        />
+                        <Redirect from="/" to="/catalog"/>
+                    </Switch>
                 </Content>
             </div>
         </Provider>
     )
 }
 
-export default App
+const AppContainer = withRouter(App)
+
+const MainApp = () => {
+    return <BrowserRouter>
+        <AppContainer/>
+    </BrowserRouter>
+}
+
+export default MainApp
