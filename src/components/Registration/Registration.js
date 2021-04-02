de7f7636f0f128a7073bb9342a652365567d16b8
VerdiFran <tmw.kva@yandex.ru>
2021-04-02 15:22:57 +0700

    Add registration from design

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,55 +1,88 @@
 import React from 'react'
 import {Formik} from 'formik'
-import {Button, Form, Input} from 'antd'
+import {Alert, Button, Form, Input, Typography} from 'antd'
+import styles from './Registration.module.scss'
 
 /**
  * Component with register form
  * @returns {JSX.Element}
  * @constructor
  */
-const Registration = ({message, register}) => {
+const Registration = ({register}) => {
+    const {Title} = Typography
+
+    const passwordFieldsNotIsEmpty = (values) => values.password.length > 0 && values.confPassword.length > 0
+
     return (
         <div>
+            <Title level={4}>Регистрация</Title>
             <Formik
                 initialValues={{
                     email: '',
                     password: '',
+                    confPassword: '',
                     passwordIsConfirmed: false
                 }}
-                onSubmit={(values) => {register(values.email, values.password)}}
+                onSubmit={(values) => {
+                    register(values.email, values.password)
+                }}
             >
                 {({values, handleSubmit, handleChange, setFieldValue}) => (
-                    <Form layout="horizontal">
-                        <Form.Item label="Email">
-                            <Input
-                                name="email"
-                                value={values.email}
-                                onChange={handleChange}
-                            />
-                        </Form.Item>
-                        <Form.Item label="Пароль">
-                            <Input.Password
-                                name="password"
-                                value={values.password}
-                                onChange={handleChange}
-                            />
-                        </Form.Item>
-                        <Form.Item label="Подтверждение пароля">
-                            <Input.Password
-                                onChange={(e) => setFieldValue(
-                                        'passwordIsConfirmed',
-                                        values.password === e.currentTarget.value
-                                    )}
-                            />
-                        </Form.Item>
-                        {
-                            values.passwordIsConfirmed && <div>Password is confirmed</div>
-                        }
-                        <div>{message}</div>
-                        <Form.Item>
-                            <Button htmlType="submit" onClick={handleSubmit}>Зарегестрироваться</Button>
-                        </Form.Item>
-                    </Form>
+                    <div className={styles.formContainer}>
+                        <Form layout="vertical">
+                            <Form.Item label="Email">
+                                <Input
+                                    name="email"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                />
+                            </Form.Item>
+                            <Form.Item label="Пароль">
+                                <Input.Password
+                                    name="password"
+                                    value={values.password}
+                                    onChange={(e) => {
+                                        setFieldValue('password', e.currentTarget.value)
+                                        setFieldValue(
+                                            'passwordIsConfirmed',
+                                            passwordFieldsNotIsEmpty(values) &&
+                                            values.confPassword === e.currentTarget.value
+                                        )
+                                    }}
+                                />
+                            </Form.Item>
+                            <Form.Item label="Подтверждение пароля">
+                                <Input.Password
+                                    onChange={(e) => {
+                                        setFieldValue('confPassword', e.currentTarget.value)
+                                        setFieldValue(
+                                            'passwordIsConfirmed',
+                                            passwordFieldsNotIsEmpty(values) &&
+                                            values.password === e.currentTarget.value
+                                        )
+                                    }}
+                                />
+                            </Form.Item>
+                            {
+                                values.passwordIsConfirmed && <Form.Item>
+                                    <Alert message="Пароль подтвержден." type="success" showIcon/>
+                                </Form.Item>
+                            }
+                            {/*<div>{message}</div>*/}
+                            {/*{
+                                messageText.length > 0 && message.error(messageText)
+                            }*/}
+                            <Form.Item>
+                                <div className={styles.buttonContainer}>
+                                    <Button
+                                        htmlType="submit"
+                                        onClick={handleSubmit}
+                                        type="primary"
+                                    >Зарегестрироваться</Button>
+                                </div>
+                            </Form.Item>
+                        </Form>
+                    </div>
                 )}
             </Formik>
         </div>
--- src/utils/selectors/authSelectors.js
+++ src/utils/selectors/authSelectors.js
@@ -1,5 +1,3 @@
 export const getIsAuth = (state) => state.auth.isAuth
 
 export const getUserData = (state) => ({email: state.auth.email})
-
-export const getMessage = (state) => state.auth.message
