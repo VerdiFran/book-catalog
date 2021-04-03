841f27a9a7286daef74d54b015d79510191e181b
VerdiFran <tmw.kva@yandex.ru>
2021-04-03 12:46:03 +0700

    Add login validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Formik} from 'formik'
 import {Button, Form, Input, Typography} from 'antd'
 import styles from './Login.module.scss'
+import * as Yup from 'yup'
 
 /**
  * Component with auth form
@@ -11,27 +12,50 @@ import styles from './Login.module.scss'
 const Login = ({login}) => {
     const {Title} = Typography
 
+    const LoginSchema = Yup.object().shape({
+        email: Yup.string().email('Неправильный email').required('Это поле обязательно для заполнения'),
+        password: Yup.string().required('Это поле обязательно для заполнения')
+    })
+
+    const [validateErrors, setValidateErrors] = useState({})
+
     return (
         <div>
             <Title level={4}>Вход</Title>
             <Formik
                 initialValues={{
-                    email: 'ttt@mail.ru',
-                    password: 'password123'
+                    email: '',
+                    password: ''
                 }}
+                validationSchema={LoginSchema}
                 onSubmit={(values) => login(values.email, values.password)}
             >
-                {({values, handleSubmit, handleChange}) => (
+                {({
+                      values,
+                      handleSubmit,
+                      handleChange,
+                      validateForm
+                  }) => (
                     <div className={styles.formContainer}>
                         <Form layout="vertical">
-                            <Form.Item label="Email">
+                            <Form.Item
+                                label="Email"
+                                validateStatus={validateErrors.email && 'error'}
+                                help={validateErrors.email}
+                                hasFeedback
+                            >
                                 <Input
                                     name="email"
                                     value={values.email}
                                     onChange={handleChange}
                                 />
                             </Form.Item>
-                            <Form.Item label="Пароль">
+                            <Form.Item
+                                label="Пароль"
+                                validateStatus={validateErrors.password && 'error'}
+                                help={validateErrors.password}
+                                hasFeedback
+                            >
                                 <Input.Password
                                     name="password"
                                     value={values.password}
@@ -40,7 +64,15 @@ const Login = ({login}) => {
                             </Form.Item>
                             <Form.Item>
                                 <div className={styles.buttonContainer}>
-                                    <Button htmlType="submit" onClick={handleSubmit} type="primary">Войти</Button>
+                                    <Button
+                                        htmlType="submit"
+                                        onClick={() => {
+                                            validateForm()
+                                                .then((errors) => setValidateErrors(errors))
+                                            handleSubmit()
+                                        }}
+                                        type="primary"
+                                    >Войти</Button>
                                 </div>
                             </Form.Item>
                         </Form>
