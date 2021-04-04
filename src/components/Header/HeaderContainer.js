f934615644445d191c341cfe4186e8edcadd99f6
VerdiFran <tmw.kva@yandex.ru>
2021-04-04 17:53:12 +0700

    Add logout function

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -1,10 +1,11 @@
 import {connect} from 'react-redux'
 import Header from './Header'
 import {getIsAuth, getUserData} from '../../utils/selectors/authSelectors'
+import {logout} from '../../redux/reducers/authReducer'
 
 const mapStateToProps = (state) => ({
     isAuth: getIsAuth(state),
     userData: getUserData(state)
 })
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps, {logout})(Header)
