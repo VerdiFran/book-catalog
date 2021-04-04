16c3a739ea018d2026850d420892c1b75669b885
VerdiFran <tmw.kva@yandex.ru>
2021-04-04 10:43:06 +0700

    Fix getCurrentBookData function in catalogSelectors.js

--- src/utils/selectors/catalogSelectors.js
+++ src/utils/selectors/catalogSelectors.js
@@ -2,6 +2,6 @@ export const getBooks = (state) => state.catalog.books
 
 export const getLoading = (state) => state.catalog.loading
 
-export const getCurrentBookData = (state) => state.catalog.currentBookInfo
+export const getCurrentBookData = (state) => state.catalog.currentBookData
 
 export const getCurrentBookIsSet = (state) => state.catalog.currentBookIsSet
